489f9180d536f21f2e96d5e34dc4327ca403c6f7
jmoney17 <wrikerjordan@gmail.com>
2022-05-15 21:54:06 -0400

    fixed ports

--- config/default.ts
+++ config/default.ts
@@ -1,7 +1,7 @@
 export default {
   port: 1337,
   origin: 'http://localhost:3000',
-  dbUri: "mongodb+srv://tutorial-admin:admin123456@cluster0.2reum.mongodb.net/rest-api-tutorial?retryWrites=true&w=majority",
+  dbUri: "",
   saltWorkFactor: 10,
   accessTokenTtl: "15m",
   refreshTokenTtl: "3h",
